ff50fb1a8202a8ff903309de68dffaa25dab2456
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-29 14:51:10 +0900

    parse command-line arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void register_cmd(char *cmd, char *pattern) {
+
+}
+
+int try_call(char *cmd, char *query) {
+
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        return 1;
+    }
+    char *sub = argv[0];
+    if (strcmp(sub, "register") == 0) {
+        register_cmd(argv[1], argv[2]);
+        return 0;
+    }
+
+    return try_call(argv[1], argv[2]);
 }
