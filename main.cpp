e0d16a6ccc03b6de093ca073abca1e88520ed296
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-30 16:17:55 +0900

    fix arguments to pass

--- main.cpp
+++ main.cpp
@@ -10,14 +10,17 @@ int try_call(char *cmd, char *query) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        return 1;
-    }
-    char *sub = argv[0];
-    if (strcmp(sub, "register") == 0) {
-        register_cmd(argv[1], argv[2]);
-        return 0;
+    if (argc > 3) {
+        char *sub = argv[1];
+        if (strcmp(sub, "register") == 0) {
+            register_cmd(argv[2], argv[3]);
+            return 0;
+        }
     }
 
-    return try_call(argv[1], argv[2]);
+    if (argc > 2) {
+        return try_call(argv[1], argv[2]);
+    }
+
+    return 1;
 }
